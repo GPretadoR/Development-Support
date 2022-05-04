5fb50e993613fbbc8479654d3322710d351ee388
Garnik Ghazaryan <garnik_ghazaryan@epam.com>
2022-05-04 21:57:50 +0400

    Initial

---
+++ Sources/Development-Support/Development_Support.swift
@@ -0,0 +1,6 @@
+public struct Development_Support {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/Development-SupportTests/Development_SupportTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Development_Support
+
+final class Development_SupportTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Development_Support().text, "Hello, World!")
+    }
+}
