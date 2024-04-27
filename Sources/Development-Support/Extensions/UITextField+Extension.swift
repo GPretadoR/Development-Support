0db57811c4d2dc3898e20eb0dedef536ac8b3174
Garnik Ghazaryan <garnik_ghazaryan@epam.com>
2024-04-28 00:59:39 +0400

    text change publishers

--- Sources/Development-Support/Extensions/UITextField+Extension.swift
+++ Sources/Development-Support/Extensions/UITextField+Extension.swift
@@ -1,8 +1,33 @@
 //
-//  File.swift
-//  
+//  UITextField+Extension.swift
+//
 //
 //  Created by Garnik Ghazaryan on 4/28/24.
 //
 
-import Foundation
+import Combine
+import UIKit
+
+extension UITextField {
+    @available(iOS 13.0, *)
+    var textChangePublisher: AnyPublisher<String, Never> {
+        NotificationCenter.default.publisher(
+            for: UITextField.textDidChangeNotification,
+            object: self
+        )
+        .compactMap { ($0.object as? UITextField)?.text }
+        .eraseToAnyPublisher()
+    }
+}
+
+extension UITextView {
+    @available(iOS 13.0, *)
+    var textChangePublisher: AnyPublisher<String, Never> {
+        NotificationCenter.default.publisher(
+            for: UITextView.textDidChangeNotification,
+            object: self
+        )
+        .compactMap { ($0.object as? UITextView)?.text }
+        .eraseToAnyPublisher()
+    }
+}
